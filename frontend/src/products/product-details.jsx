a0fd5e1e3c0e6122fa424461134de45bf1bf2f2f
= <=>
2020-12-06 16:50:03 +0530

    adding more css

--- frontend/src/products/product-details.jsx
+++ frontend/src/products/product-details.jsx
@@ -51,7 +51,7 @@ export class ProductDetailsComponent extends React.Component {
     let placeOrderHTML = '';
 
     if (!this.state.showOrderForm) {
-      placeOrderHTML = <Button variant="contained" color="primary" onClick={() => this.placeOrder.bind(this)(true)} >Place Order</Button>
+      placeOrderHTML = <div><Button variant="contained" color="primary" onClick={() => this.placeOrder.bind(this)(true)} >Place Order</Button></div>
     } else if (this.state.showOrderForm && !this.state.orderForm.confimed) {
       placeOrderHTML = <div>
         <FormControl className={styles.element_row}>
@@ -64,8 +64,10 @@ export class ProductDetailsComponent extends React.Component {
           <Input id="my-input" aria-describedby="my-helper-text" onChange={(event) => this.handleChange('address', event)} />
           <FormHelperText id="my-helper-text">Shipping address</FormHelperText>
         </FormControl>
-        <Button variant="contained" color="primary" onClick={() => this.placeOrder.bind(this)(false)}> Cancel</Button>
-        <Button variant="contained" color="primary" onClick={() => this.handleChange('confimed', true)}> Confirm And Pay</Button>
+        <div>
+          <Button variant="contained" color="primary" onClick={() => this.placeOrder.bind(this)(false)}> Cancel</Button>
+          <Button variant="contained" color="primary" onClick={() => this.handleChange('confimed', true)}> Confirm And Pay</Button>
+        </div>
       </div>
       // <span className={styles.product_specs_label}> <b>Note:</b> Place an order by msg 7022336741 </span>
     } else if (this.state.showOrderForm && this.state.orderForm.confimed) {
@@ -86,24 +88,25 @@ export class ProductDetailsComponent extends React.Component {
     if (this.state.productLoaded) {
       const alImages = this.state.selectedProduct.productImages || [];
       productDetailsHTML = <div className={styles.produt_details_body}>
+        <div className={styles.product_specs_name}>{this.state.selectedProduct.name}</div>
         <div className={styles.product_image}>
           <img className={styles.product_selected_image} src={this.state.selectedAltImgPath}></img>
         </div>
         <div className={styles.product_info}>
           <div className={styles.product_specs}>
-            <span className={styles.product_specs_label}>{this.state.selectedProduct.name}</span>
-            <span className={styles.product_specs_label}>{this.state.selectedProduct.description}</span>
+            <p> <b>Details: </b></p>
+            <p className={styles.product_specs_label}>{this.state.selectedProduct.description}</p>
             <span className={styles.product_specs_label}>{this.state.selectedProduct.cost}</span>
-            <span className={styles.product_specs_label}> ₹  {this.state.selectedProduct.price}</span>
-            {placeOrderHTML}
-          </div>
-          <div className={styles.similar_products}>
-            <div className={styles.mobile_scroll_width}>
-              {alImages.map((altImg, i) => <img onClick={() => this.onAltImgClick.bind(this)(i)} key={i} className={styles.product_alt_img_tile} src={altImg.path}></img>)}
-            </div>
+            <span className={styles.product_specs_label, styles.product_specs_price}> ₹  {this.state.selectedProduct.price} + Delivery changes(100Rs) </span>
+          {placeOrderHTML}
+        </div>
+        <div className={styles.similar_products}>
+          <div className={styles.mobile_scroll_width}>
+            {alImages.map((altImg, i) => <img onClick={() => this.onAltImgClick.bind(this)(i)} key={i} className={styles.product_alt_img_tile} src={altImg.path}></img>)}
           </div>
         </div>
-      </div>
+        </div >
+      </div >
     }
 
 
